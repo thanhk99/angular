06eff52de5ab2aef65d81d0010a8827cc925672e
thanhk99 <thanh040804@gmail.com>
2025-01-07 20:54:50 +0700

    them header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
     constructor(private router:Router){}
-    title="thanhdz"
-    gotoHome(){
-      this.router.navigateByUrl('/about');
-    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
-    { path: 'about', component: AboutComponent }, // Thêm route cho trang About
+    { path: 'about', component: AboutComponent },
     { path: '', component:HomeComponent },
   ];
